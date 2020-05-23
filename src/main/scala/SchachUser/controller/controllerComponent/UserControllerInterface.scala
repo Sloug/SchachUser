bc93b64104884253160423144a2df704104ef600
Sloug <carrylol@aol.com>
2020-05-23 13:23:35 +0200

    reworked the api

--- src/main/scala/SchachUser/controller/controllerComponent/UserControllerInterface.scala
+++ src/main/scala/SchachUser/controller/controllerComponent/UserControllerInterface.scala
@@ -2,14 +2,22 @@ package SchachUser.controller.controllerComponent
 
 import SchachUser.model.userComponent.userBaseImpl.UserState.UserState
 
+
+//names
+//whos turn
+//next (only choose if move was valid, if promotion after promotion)
 trait UserControllerInterface {
-  def name: String
+  def names: (String, String) //2
 
-  def colour: Boolean
+  def colour: Boolean //weg
 
-  def doRound: Boolean
+  def nextRound: Boolean // maybe return state
 
   def myTurn: Boolean
 
-  def state: UserState
+  def state: UserState //weg
+
+  def setRemi
+
+  def setWonLost
 }
--- src/main/scala/SchachUser/controller/controllerComponent/controllerBaseImpl/UserController.scala
+++ src/main/scala/SchachUser/controller/controllerComponent/controllerBaseImpl/UserController.scala
@@ -5,11 +5,11 @@ import SchachUser.model.userComponent.userBaseImpl.User
 
 case class UserController(var user: User) extends UserControllerInterface {
 
-  override def name: String = user.name
+  override def names: (String, String) = (user.name, "")
 
   override def colour: Boolean = user.black
 
-  override def doRound: Boolean = if (user.myTurn) {
+  override def nextRound: Boolean = if (user.myTurn) {
     user.myTurn = false
     true
   } else false
@@ -17,4 +17,8 @@ case class UserController(var user: User) extends UserControllerInterface {
   override def myTurn: Boolean = user.myTurn
 
   override def state: _root_.SchachUser.model.userComponent.userBaseImpl.UserState.UserState = user.state
+
+  override def setRemi: Unit = ???
+
+  override def setWonLost: Unit = ???
 }
